888555bf2532fafb985c8b9b01dc9dc3c6c53bf9
Kaloyan Enev <darthkylorenenev@gmail.com>
2025-07-15 15:38:40 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,256 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.insert_btn = new System.Windows.Forms.Button();
+            this.update_btn = new System.Windows.Forms.Button();
+            this.delete_btn = new System.Windows.Forms.Button();
+            this.report_btn = new System.Windows.Forms.Button();
+            this.display_all_btn = new System.Windows.Forms.Button();
+            this.txt_name = new System.Windows.Forms.TextBox();
+            this.txt_state = new System.Windows.Forms.TextBox();
+            this.txt_report = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.database1DataSet = new WindowsFormsApp1.Database1DataSet();
+            this.testBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.testTableAdapter = new WindowsFormsApp1.Database1DataSetTableAdapters.TestTableAdapter();
+            this.idDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.stateDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.database1DataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.testBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.idDataGridViewTextBoxColumn,
+            this.nameDataGridViewTextBoxColumn,
+            this.stateDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.testBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(84, 140);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(618, 150);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.RowHeaderMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dataGridView1_RowHeaderMouseClick);
+            // 
+            // insert_btn
+            // 
+            this.insert_btn.Location = new System.Drawing.Point(361, 46);
+            this.insert_btn.Name = "insert_btn";
+            this.insert_btn.Size = new System.Drawing.Size(75, 23);
+            this.insert_btn.TabIndex = 1;
+            this.insert_btn.Text = "Insert";
+            this.insert_btn.UseVisualStyleBackColor = true;
+            this.insert_btn.Click += new System.EventHandler(this.insert_btn_Click);
+            // 
+            // update_btn
+            // 
+            this.update_btn.Location = new System.Drawing.Point(495, 46);
+            this.update_btn.Name = "update_btn";
+            this.update_btn.Size = new System.Drawing.Size(75, 23);
+            this.update_btn.TabIndex = 2;
+            this.update_btn.Text = "Update";
+            this.update_btn.UseVisualStyleBackColor = true;
+            this.update_btn.Click += new System.EventHandler(this.update_btn_Click);
+            // 
+            // delete_btn
+            // 
+            this.delete_btn.Location = new System.Drawing.Point(627, 46);
+            this.delete_btn.Name = "delete_btn";
+            this.delete_btn.Size = new System.Drawing.Size(75, 23);
+            this.delete_btn.TabIndex = 3;
+            this.delete_btn.Text = "Delete";
+            this.delete_btn.UseVisualStyleBackColor = true;
+            // 
+            // report_btn
+            // 
+            this.report_btn.Location = new System.Drawing.Point(364, 360);
+            this.report_btn.Name = "report_btn";
+            this.report_btn.Size = new System.Drawing.Size(75, 23);
+            this.report_btn.TabIndex = 4;
+            this.report_btn.Text = "Report";
+            this.report_btn.UseVisualStyleBackColor = true;
+            // 
+            // display_all_btn
+            // 
+            this.display_all_btn.Location = new System.Drawing.Point(531, 360);
+            this.display_all_btn.Name = "display_all_btn";
+            this.display_all_btn.Size = new System.Drawing.Size(99, 23);
+            this.display_all_btn.TabIndex = 5;
+            this.display_all_btn.Text = "Display all";
+            this.display_all_btn.UseVisualStyleBackColor = true;
+            // 
+            // txt_name
+            // 
+            this.txt_name.Location = new System.Drawing.Point(137, 24);
+            this.txt_name.Name = "txt_name";
+            this.txt_name.Size = new System.Drawing.Size(100, 22);
+            this.txt_name.TabIndex = 6;
+            // 
+            // txt_state
+            // 
+            this.txt_state.Location = new System.Drawing.Point(137, 72);
+            this.txt_state.Name = "txt_state";
+            this.txt_state.Size = new System.Drawing.Size(100, 22);
+            this.txt_state.TabIndex = 7;
+            // 
+            // txt_report
+            // 
+            this.txt_report.Location = new System.Drawing.Point(165, 361);
+            this.txt_report.Name = "txt_report";
+            this.txt_report.Size = new System.Drawing.Size(100, 22);
+            this.txt_report.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(34, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(34, 75);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 16);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "State";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(52, 367);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(76, 16);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "State to find";
+            // 
+            // database1DataSet
+            // 
+            this.database1DataSet.DataSetName = "Database1DataSet";
+            this.database1DataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // testBindingSource
+            // 
+            this.testBindingSource.DataMember = "Test";
+            this.testBindingSource.DataSource = this.database1DataSet;
+            // 
+            // testTableAdapter
+            // 
+            this.testTableAdapter.ClearBeforeFill = true;
+            // 
+            // idDataGridViewTextBoxColumn
+            // 
+            this.idDataGridViewTextBoxColumn.DataPropertyName = "Id";
+            this.idDataGridViewTextBoxColumn.HeaderText = "Id";
+            this.idDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.idDataGridViewTextBoxColumn.Name = "idDataGridViewTextBoxColumn";
+            this.idDataGridViewTextBoxColumn.ReadOnly = true;
+            this.idDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // nameDataGridViewTextBoxColumn
+            // 
+            this.nameDataGridViewTextBoxColumn.DataPropertyName = "Name";
+            this.nameDataGridViewTextBoxColumn.HeaderText = "Name";
+            this.nameDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.nameDataGridViewTextBoxColumn.Name = "nameDataGridViewTextBoxColumn";
+            this.nameDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // stateDataGridViewTextBoxColumn
+            // 
+            this.stateDataGridViewTextBoxColumn.DataPropertyName = "State";
+            this.stateDataGridViewTextBoxColumn.HeaderText = "State";
+            this.stateDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.stateDataGridViewTextBoxColumn.Name = "stateDataGridViewTextBoxColumn";
+            this.stateDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txt_report);
+            this.Controls.Add(this.txt_state);
+            this.Controls.Add(this.txt_name);
+            this.Controls.Add(this.display_all_btn);
+            this.Controls.Add(this.report_btn);
+            this.Controls.Add(this.delete_btn);
+            this.Controls.Add(this.update_btn);
+            this.Controls.Add(this.insert_btn);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.database1DataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.testBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button insert_btn;
+        private System.Windows.Forms.Button update_btn;
+        private System.Windows.Forms.Button delete_btn;
+        private System.Windows.Forms.Button report_btn;
+        private System.Windows.Forms.Button display_all_btn;
+        private System.Windows.Forms.TextBox txt_name;
+        private System.Windows.Forms.TextBox txt_state;
+        private System.Windows.Forms.TextBox txt_report;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private Database1DataSet database1DataSet;
+        private System.Windows.Forms.BindingSource testBindingSource;
+        private Database1DataSetTableAdapters.TestTableAdapter testTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn idDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn stateDataGridViewTextBoxColumn;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string cs = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\evasi\Desktop\UNI\Current Assignments\ST\WindowsFormsApp1\Database1.mdf;Integrated Security=True";
+        SqlConnection sqlconnection;
+        SqlCommand sqlcommand;
+        string Query;
+        DataTable datatable;
+        SqlDataAdapter sqladapter;
+        int ID = 0;
+
+        private void DisplayData()
+        {
+            sqlconnection = new SqlConnection(cs);
+            Query = "Select * From Test";
+            sqlcommand = new SqlCommand(Query, sqlconnection);
+            sqladapter = new SqlDataAdapter();
+            datatable = new DataTable();
+            sqladapter.SelectCommand = sqlcommand;
+            sqladapter.Fill(datatable);
+            dataGridView1.DataSource = datatable;
+        }
+
+        private void ClearData()
+        {
+            txt_name.Text = " ";
+            txt_state.Text = " ";
+            ID = 0;
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'database1DataSet.Test' table. You can move, or remove it, as needed.
+            this.testTableAdapter.Fill(this.database1DataSet.Test);
+
+        }
+
+        private void insert_btn_Click(object sender, EventArgs e)
+        {
+            if (txt_name.Text != " " && txt_state.Text != " ")
+            {
+                sqlconnection = new SqlConnection(cs);
+                sqlconnection.Open();
+                Query = "Insert INTO Test(name,state) " + "VALUES(@name,@state)";
+                sqlcommand = new SqlCommand(Query,sqlconnection);
+                sqlcommand.Parameters.AddWithValue("@name", txt_name.Text);
+                sqlcommand.Parameters.AddWithValue("@state", txt_state.Text);
+                sqlcommand.ExecuteNonQuery();
+                sqlconnection.Close();
+                MessageBox.Show("Insert successfully!");
+                DisplayData();
+                ClearData();
+            }
+        }
+
+        private void dataGridView1_RowHeaderMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            ID = Convert.ToInt32(dataGridView1.Rows[e.RowIndex].Cells[0].Value);
+            txt_name.Text = dataGridView1.Rows[e.RowIndex].Cells[1].Value.ToString();
+            txt_state.Text = dataGridView1.Rows[e.RowIndex].Cells[2].Value.ToString();
+
+        }
+
+        private void update_btn_Click(object sender, EventArgs e)
+        {
+            if (txt_name.Text != " " && txt_state.Text != " ")
+            {
+                sqlconnection = new SqlConnection(cs);
+                sqlconnection.Open();
+                Query = "Update Test Set name=@name, state = @state Where Id = @id";
+                sqlcommand = new SqlCommand(Query, sqlconnection);
+                sqlcommand.Parameters.AddWithValue("@id", ID);
+                sqlcommand.Parameters.AddWithValue("@name", txt_name.Text);
+                sqlcommand.Parameters.AddWithValue("@state", txt_state.Text);
+                sqlcommand.ExecuteNonQuery();
+                sqlconnection.Close();
+                MessageBox.Show("Record updated successfully!");
+                DisplayData();
+                ClearData();
+            }
+        }
+    }
+}
